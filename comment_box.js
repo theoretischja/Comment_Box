515199ae7f5f53e683ca7193170b3c0eca633d59
Em-Steen <emma.c_steen@me.com>
2022-08-17 10:54:18 +0100

    new js file with pseudocode for character iteration function

---
+++ comment_box.js
@@ -0,0 +1,13 @@
+
+/*  FAC REQUIREMENTS
+    1. Check whether the comment exceeds a character count of 140 characters using Javascript. 
+    2. Display the number of characters as the user types (e.g. 87/140)
+    3. Give user feedback with custom styling on the comment box, when the user exceeds the 
+    character limit (e.g. with a coloured border)*/
+
+/*  1.
+    - function that iterates through the characters while the user types, and displays iterations
+    on the website (before fixed /140)
+    - once it's iterated through 140 characters, display error message and custom box styling 
+    (stretch goal: don't allow more characters to be typed)
+    */
