026d78e4f7cbf1d4b8cd087bf6b04ce950249bc6
Em-Steen <emma.c_steen@me.com>
2022-08-19 18:43:15 +0100

    js to retrieve user input and more pseudocode

--- comment_box.js
+++ comment_box.js
@@ -1,13 +1,14 @@
 
-/*  FAC REQUIREMENTS
-    1. Check whether the comment exceeds a character count of 140 characters using Javascript. 
-    2. Display the number of characters as the user types (e.g. 87/140)
-    3. Give user feedback with custom styling on the comment box, when the user exceeds the 
-    character limit (e.g. with a coloured border)*/
+//retrieving user input from the comment box and defining it as a variable
+  
+let input = document.getElementById("comment").value;
 
-/*  1.
-    - function that iterates through the characters while the user types, and displays iterations
-    on the website (before fixed /140)
-    - once it's iterated through 140 characters, display error message and custom box styling 
-    (stretch goal: don't allow more characters to be typed)
-    */
+//returning input on website
+
+//checking if input exceeds 14 chars, and if so, enacting special CSS styling of box
+  // if (input > 140) {
+       //enact CSS styling of box
+   //}
+
+//displaying number of chars as the user types (e.g. 87/140)
+//- function that iterates through the characters while the user types, and displays iterations?
