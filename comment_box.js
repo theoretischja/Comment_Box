be1fc8d64e96e3a0a0a175bff495a9e7532e2335
Em-Steen <emma.c_steen@me.com>
2022-09-06 19:47:56 +0100

    added onsubmit event listener for displayComment() function, to display user comment on page after submitting

--- comment_box.js
+++ comment_box.js
@@ -4,30 +4,32 @@
 //Give user feedback with custom styling on the comment box, when the user exceeds the character limit (e.g. with a coloured border)
 //Show the comment on the page when submitted
 
-//create a variable for the userinput - this currently gets the fixed value in index.html
-let userInput = document.getElementById("comment").value;
-
-//create function that shows user input on page - working
+//create function that shows user input on page when submitted; create a variable for the userinput - almost working; input does not remain on page after submitting
+//WORKS except problem: form is cleared after submitting
 function displayComment() {
-  document.getElementById('showComment').innerHTML = userInput;
+  let userInput = document.getElementById("comment").value;
+  document.getElementById("showComment").innerHTML = 'Your comment says: ' + userInput;
 }
 
 displayComment();
 
-//create function that checks whether the comment exceeds 140 chars - this works only when value="" in index.html
-const charCount = (userInput) => {
-  if (userInput > 1) {
-    console.log('You have exceeded the character count limit of 140 characters.');
-  }
-}
-
-//charCount();
-
 //function that displays number of chars as the user types (e.g. 87/140)
+//eventlistener for every key pressed in input box
 //function iterate() {
   //for (let i=0; i < 140, i++;) {
   // return i;
   //}
+
+//create function that checks whether the comment exceeds 140 chars - this works only when value="" in index.html
+//const charCount = (userInput) => {
+  //if (userInput > 1) {
+    //console.log('You have exceeded the character count limit of 140 characters.');
+  //}
+//}
+
+//charCount();
+
+
 
   //function that checks if input exceeds 140 chars, and if so, enacts special CSS styling of box
 //function charExceed() {
