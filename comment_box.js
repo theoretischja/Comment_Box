64f48aced49219d257840f8b4eab7a5409e4e8e7
Em-Steen <emma.c_steen@me.com>
2022-09-07 22:01:28 +0100

    added eventlisteners and functions for comment to be displayed when submitted (working); for character count being shown onchange (not yet working); for checking if char count exceeds char limit (not yet working)

--- comment_box.js
+++ comment_box.js
@@ -1,45 +1,37 @@
 
-//Check whether the comment exceeds a character count of 140 characters using Javascript
-//Display the number of characters as the user types (e.g. 87/140)
+//WIP - Check whether the comment exceeds a character count of 140 characters using Javascript
+//WIP - Display the number of characters as the user types (e.g. 87/140)
 //Give user feedback with custom styling on the comment box, when the user exceeds the character limit (e.g. with a coloured border)
-//Show the comment on the page when submitted
+//DONE - Show the comment on the page when submitted
 
-//create function that shows user input on page when submitted; create a variable for the userinput - almost working; input does not remain on page after submitting
-//WORKS except problem: form is cleared after submitting
+//create function that shows user input on page when submitted; create a variable for the userinput
+
+let userInput = document.getElementById("comment").value;
+
 function displayComment() {
   let userInput = document.getElementById("comment").value;
   document.getElementById("showComment").innerHTML = 'Your comment says: ' + userInput;
 }
 
-displayComment();
-
 //function that displays number of chars as the user types (e.g. 87/140)
-//eventlistener for every key pressed in input box
-//function iterate() {
-  //for (let i=0; i < 140, i++;) {
-  // return i;
-  //}
+//WORKS except: chars doesn't get input length. Eventlistener seems to be correct.
 
-//create function that checks whether the comment exceeds 140 chars - this works only when value="" in index.html
-//const charCount = (userInput) => {
-  //if (userInput > 1) {
-    //console.log('You have exceeded the character count limit of 140 characters.');
-  //}
-//}
-
-//charCount();
-
+function charCount() {
+  let chars = userInput.length;
+  document.getElementById("charCount").innerHTML = `Character count: ${chars} /140.`;
+}
+//create function that checks whether the comment exceeds 140 chars
+//this MIGHT work once chars actually gets the input length (see function above)
+function charExceed() {
+  if (chars > 140) {
+    document.getElementById("charExceed").innerHTML = 'You have exceeded the character count limit of 140 characters.';
+  }
+}
 
 
   //function that checks if input exceeds 140 chars, and if so, enacts special CSS styling of box
 //function charExceed() {
- //if  >//(html maxlength is reached) {
+ //if  >//(chars > 140) {
   // //enact CSS styling of box
   //}
 //}
-//create a function that shows the comment on the page when submitted
-//function printComment() {
-  //console.log(userInput);
-//}
-
-//printComment();
