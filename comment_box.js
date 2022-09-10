2ad49ffbd4c2ee90405ea0eb173798d6aab21d71
Em-Steen <emma.c_steen@me.com>
2022-09-10 20:08:09 +0100

    some notes on userInput.length logging = 0

--- comment_box.js
+++ comment_box.js
@@ -5,8 +5,11 @@
 //DONE - Show the comment on the page when submitted
 
 //create function that shows user input on page when submitted; create a variable for the userinput
+//seems to work BUT when logging userInput.length it is =0. Why?
 
+//Do these variables outside functions need to be called in some way in the HTML to work?
 let userInput = document.getElementById("comment").value;
+let chars = userInput.length;
 
 function displayComment() {
   let userInput = document.getElementById("comment").value;
@@ -14,12 +17,14 @@ function displayComment() {
 }
 
 //function that displays number of chars as the user types (e.g. 87/140)
-//WORKS except: chars doesn't get input length. Eventlistener seems to be correct.
+//WORKS except: input length is 0. Yet input prints in displayComment.
 
 function charCount() {
   let chars = userInput.length;
-  document.getElementById("charCount").innerHTML = `Character count: ${chars} /140.`;
+  //while loop here?
+  document.getElementById("charCount").innerHTML = 'Character count: ' + chars + '/140.';
 }
+
 //create function that checks whether the comment exceeds 140 chars
 //this MIGHT work once chars actually gets the input length (see function above)
 function charExceed() {
