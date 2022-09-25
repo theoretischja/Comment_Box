325828adc12c4641bb3806f233a315753a6f36ff
Em-Steen <emma.c_steen@me.com>
2022-09-25 19:05:10 +0100

    overhaul of JS, most functions now working; plus addition of css file

--- comment_box.js
+++ comment_box.js
@@ -1,42 +1,48 @@
 
-//WIP - Check whether the comment exceeds a character count of 140 characters using Javascript
-//WIP - Display the number of characters as the user types (e.g. 87/140)
+//Check whether the comment exceeds a character count of 140 characters using Javascript
+// - works, but deletions are also counted as characters
+//Display the number of characters as the user types (e.g. 87/140)
+// - works
 //Give user feedback with custom styling on the comment box, when the user exceeds the character limit (e.g. with a coloured border)
-//DONE - Show the comment on the page when submitted
+// - works
+//Show the comment on the page when submitted alongside the author’s name
+// - works but need to prevent default so form doesn't reset
+//Allow the user to submit additional comments, while previous comments remain visible
 
-//create function that shows user input on page when submitted; create a variable for the userinput
-//seems to work BUT when logging userInput.length it is =0. Why?
 
-//Do these variables outside functions need to be called in some way in the HTML to work?
-let userInput = document.getElementById("comment").value;
-let chars = userInput.length;
+let char = document.getElementById("comment").value.length;
+let comment = document.getElementById("comment");
+let showComment = document.getElementById("showComment");
+let charCount = document.getElementById("charCount");
+let charExceed = document.getElementById("charExceed");
+let submitButton = document.getElementById("submit");
+let userName = document.getElementById("name");
+let showUserName = document.getElementById("showUserName");
 
-function displayComment() {
-  let userInput = document.getElementById("comment").value;
-  document.getElementById("showComment").innerHTML = 'Your comment says: ' + userInput;
-}
-
-//function that displays number of chars as the user types (e.g. 87/140)
-//WORKS except: input length is 0. Yet input prints in displayComment.
-
-function charCount() {
-  let chars = userInput.length;
-  //while loop here?
-  document.getElementById("charCount").innerHTML = 'Character count: ' + chars + '/140.';
-}
-
-//create function that checks whether the comment exceeds 140 chars
-//this MIGHT work once chars actually gets the input length (see function above)
-function charExceed() {
-  if (chars > 140) {
-    document.getElementById("charExceed").innerHTML = 'You have exceeded the character count limit of 140 characters.';
+function charCounting() {
+  if (char > 138) {
+    charExceed.innerHTML = 'You have exceeded the word count.'
+    comment.style.border = '10px solid blue';
+  } else {
+    console.log('Goodbye!');
   }
-}
+  char++;
+};
 
+function showCharCount() {
+  charCount.innerHTML = 'Character count: ' + char + '/140';
+};
 
-  //function that checks if input exceeds 140 chars, and if so, enacts special CSS styling of box
-//function charExceed() {
- //if  >//(chars > 140) {
-  // //enact CSS styling of box
-  //}
+function submitComment() {
+  showUserName.innerHTML = userName.value;
+  showComment.innerHTML = comment.value;
+};
+
+//not in use at the moment
+//function decrementCharCount() {
+  //showComment.innerHTML = char--;
 //}
+
+comment.addEventListener('input', charCounting);
+comment.addEventListener('input', showCharCount);
+submitButton.addEventListener('click', submitComment);
