5cf9d36e5942ccac85454571f130a61d79fb8fd2
Em-Steen <emma.c_steen@me.com>
2022-09-27 18:22:11 +0100

    added js to make input fields clear after submit

--- comment_box.js
+++ comment_box.js
@@ -8,7 +8,7 @@
 //Show the comment on the page when submitted alongside the author’s name
 // - works
 //Allow the user to submit additional comments, while previous comments remain visible
-//- not working yet (need to get input fields to clear after first input, and find way to print additional comments)
+//- not working yet (find way to print additional comments)
 
 
 let char = document.getElementById("comment");
@@ -21,7 +21,7 @@ let userName = document.getElementById("name");
 let showUserName = document.getElementById("showUserName");
 
 function charCounting() {
-  if (char.value.length > 140) {
+  if (char.value.length > 14) {
     charExceed.innerHTML = 'You have exceeded the word count.'
     comment.style.border = '5px dotted red';
   } else {
@@ -35,9 +35,10 @@ function showCharCount() {
 };
 
 function submitComment(e) {
+  e.preventDefault();
   showUserName.innerHTML = 'Your name: ' + userName.value;
   showComment.innerHTML = 'Your comment: ' + comment.value;
-  e.preventDefault();
+  document.getElementById("form").reset();
 };
 
 comment.addEventListener('input', charCounting);
